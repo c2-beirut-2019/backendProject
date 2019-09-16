01ba25c974a945f9e874f66f0932c0fd6d72579d
natachaazar <natacha.a@euriskomobility.com>
2019-09-16 20:10:12 +0300

    fixer un bug

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongodb: {
-      database: 'mongodb://localhost:27017/AnimalHouse',
-      //database: 'mongodb://admin:d2RtE28MUV9AZXqc@localhost/AnimalHouse?authSource=admin',
+      //database: 'mongodb://localhost:27017/AnimalHouse',
+      database: 'mongodb://admin:d2RtE28MUV9AZXqc@localhost/AnimalHouse?authSource=admin',
       options:{}
     },
     port:4444,
